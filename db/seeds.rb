c582c4324d16dd05d31fe1e3c32122ad8fd1d6ae
Abraham Aviles <aaaviles1995@gmail.com>
2021-02-08 11:01:47 -0500

    commit tree 896063472286ea7beba7eb02da116d9c6f18ee11

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+names = ["one", "two", "three", "four", "five", "six", "seven"]
+x = 0
+names.length.times do
+    temp = Student.create(
+        first_name: names[x],
+        last_name: names[x]
+    )
+    x += 1
+end
